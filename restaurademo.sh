19f5e43788884ff946305ce1561800c42c826909
René Solis <IoTeacher@users.noreply.github.com>
2018-09-24 01:25:50 -0700

    Update restaurademo.sh

--- restaurademo.sh
+++ restaurademo.sh
@@ -1,3 +1,4 @@
+clear
 echo '     |\_/|        ****************************    (\_/) '
 echo '    / @ @ \       *   Programa que restaura  *   (='.'=) '
 echo '   ( > º < )      *       BASE DE DATOS      *   (")_(") '
@@ -5,12 +6,13 @@ echo '    `>>x<<´       *        DEMO de MYSQL     *           '
 echo '    /  O  \       ****************************           '
 echo
 echo
-echo ' FAVOR DE CAMBIAR LA IP '
+echo ' Copiar este archivo remoto en: '
+echo ' $ scp -i llavesita510.pem restaurademo.sh ubuntu@52.91.45.209:/home/ubuntu/ '
 pause
-scp -i llavesita510.pem restaurademo.sh ubuntu@52.91.45.209:/home/ubuntu/
 echo '--- Por el momento no poner APT UPGRADE --- Para salir CTRL + C ---- '
 sudo apt update
 cd
+echo '--- Creando LAMP directamente poner password prepa123 ---- '
 sudo tasksel install lamp-server
 git clone https://github.com/datacharmer/test_db
 mysql -t < employees.sql -p prepa123
