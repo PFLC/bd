599ce7173714ae1e7e72d3c92afa813476aa353b
René Solis <IoTeacher@users.noreply.github.com>
2018-09-24 01:52:26 -0700

    Update restaurademo.sh

--- restaurademo.sh
+++ restaurademo.sh
@@ -4,8 +4,8 @@ echo '    / @ @ \       *   Programa que restaura  *   (='.'=) '
 echo '   ( > º < )      *       BASE DE DATOS      *   (")_(") '
 echo '    `>>x<<´       *        DEMO de MYSQL     *           '
 echo '    /  O  \       ****************************           '
-echo
-echo
+echo ' '
+echo ' '
 echo ' Copiar este archivo remoto en: '
 echo ' $ scp -i llavesita510.pem restaurademo.sh ubuntu@52.91.45.209:/home/ubuntu/ '
 pause
@@ -13,9 +13,27 @@ echo '--- Por el momento no poner APT UPGRADE --- Para salir CTRL + C ---- '
 sudo apt update
 cd
 echo '--- Creando LAMP directamente poner password prepa123 ---- '
-sudo tasksel install lamp-server
+sudo apt install tasksel
+sudo tasksel
+clear
+
+echo ' $ sudo mysql_secure_installation       '
+echo ' -----     '
+echo ' Cambiaremos el Password de Mysql     '
+echo ' prepa123      '
+echo '      '
+echo ' Remove anonymous users?  YES    '
+echo ' Disallow root login remotely?  YES     '
+echo ' Remove test database and access to it? YES'
+echo ' Reload privilege tables now? YES '
+echo '-----------------------'
+pause
+sudo mysql_secure_installation
+
 git clone https://github.com/datacharmer/test_db
+cd test_db
+echo '---- Restaurando -----'
 mysql -t < employees.sql -p prepa123
 time mysql -t < test_employees_sha.sql
-mysql -u root -p prepa123
+mysql -u root -p 
 echo '---- Terminado todo por Default ----'
