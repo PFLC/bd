92776a9532256566462047e1600b0bd0eaedc3c2
René Solis <IoTeacher@users.noreply.github.com>
2018-09-24 01:16:31 -0700

    Update restaurademo.sh

--- restaurademo.sh
+++ restaurademo.sh
@@ -3,7 +3,12 @@ echo     / @ @ \       *   Programa que restaura  *   (='.'=)
 echo    ( > º < )      *       BASE DE DATOS      *   (")_(")
 echo     `>>x<<´       *        DEMO de MYSQL     *
 echo     /  O  \       ****************************
+echo
+echo
+echo  FAVOR DE CAMBIAR LA IP
 pause
+scp -i llavesita510.pem restaurademo.sh ubuntu@52.91.45.209:/home/ubuntu/
+echo --- Por el momento no poner APT UPGRADE --- Para salir CTRL + C ----
 sudo apt update
 cd
 sudo tasksel install lamp-server
