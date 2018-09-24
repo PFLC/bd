03e63040a6c351962a727cb60bce0e3e437fa627
René Solis <IoTeacher@users.noreply.github.com>
2018-09-24 01:18:34 -0700

    Update restaurademo.sh

--- restaurademo.sh
+++ restaurademo.sh
@@ -1,14 +1,14 @@
-echo      |\_/|        ****************************    (\_/)
-echo     / @ @ \       *   Programa que restaura  *   (='.'=)
-echo    ( > º < )      *       BASE DE DATOS      *   (")_(")
-echo     `>>x<<´       *        DEMO de MYSQL     *
-echo     /  O  \       ****************************
+echo '     |\_/|        ****************************    (\_/) '
+echo '    / @ @ \       *   Programa que restaura  *   (='.'=) '
+echo '   ( > º < )      *       BASE DE DATOS      *   (")_(") '
+echo '    `>>x<<´       *        DEMO de MYSQL     *           '
+echo '    /  O  \       ****************************           '
 echo
 echo
-echo  FAVOR DE CAMBIAR LA IP
+echo ' FAVOR DE CAMBIAR LA IP '
 pause
 scp -i llavesita510.pem restaurademo.sh ubuntu@52.91.45.209:/home/ubuntu/
-echo --- Por el momento no poner APT UPGRADE --- Para salir CTRL + C ----
+echo '--- Por el momento no poner APT UPGRADE --- Para salir CTRL + C ---- '
 sudo apt update
 cd
 sudo tasksel install lamp-server
@@ -16,4 +16,4 @@ git clone https://github.com/datacharmer/test_db
 mysql -t < employees.sql -p prepa123
 time mysql -t < test_employees_sha.sql
 mysql -u root -p prepa123
-echo ---- Terminado todo por Default ----
+echo '---- Terminado todo por Default ----'
